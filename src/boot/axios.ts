7a6b93a95a2d3b584e8692f6ff0cf15a21cb1ee0
Katanon Krengsomrong <66160196@go.buu.ac.th>
2025-03-16 05:24:48 +0700

    Update axios.ts

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -16,7 +16,7 @@ declare module 'vue' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: import.meta.env.VITE_API || 'http://localhost:3000' });
+const api = axios.create({ baseURL: import.meta.env.VITE_API ?? 'http://localhost:3000' });
 // function sleep(ms: number) {
 //   return new Promise((resolve) => setTimeout(resolve,ms))
 // }
